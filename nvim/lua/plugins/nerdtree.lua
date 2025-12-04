5ab95aa8bc2bf3cb9ecccd19743bfee152a46069
rishi-10x <rishi@10xconstruction.ai>
2025-12-04 16:13:07 +0530

    added nvim config

---
+++ nvim/lua/plugins/nerdtree.lua
@@ -0,0 +1,5 @@
+vim.keymap.set('n', '<leader>n', ':NERDTreeToggle<CR>', { desc = 'Toggle NERDTree' })
+vim.keymap.set('n', '<leader>f', ':NERDTreeFind<CR>', { desc = 'Toggle NERDTree' })
+return {
+    "preservim/nerdtree"
+}
---
+++ nvim/lua/plugins/tagbar.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n', '<F8>', ':TagbarToggle<CR>', {desc='Toggle Tagbar'})
+return {
+    "preservim/tagbar"
+}
